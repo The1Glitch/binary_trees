9b43a277ef137d7ee875375c9a732c883ae0147c
The1Glitch <easytmashabela@gamil.com>
2024-03-28 14:11:14 -0700

    Root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - checks if the node is a root of the binary tree
- * @node: The pointer of the thr node to check
+ * binary_tree_is_root - checks if the node is a root of a binary tree
+ * @node: The pointer to the node to check
  *
- * Return: if the node is a root - 1
+ * Return: if the node is the root - 1
  *         otherwise - 0
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent != NULL)
-		return(0);
+		return (0);
 
 	return (1);
 }
